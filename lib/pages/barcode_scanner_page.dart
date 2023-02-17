a86f06ab138b6d3c35c112a2640345a6bb10136f
bastianfbr <badmaxyt@gmail.com>
2023-02-18 00:10:41 +0100

    🎨 🔥 Use product and delete logic

--- lib/pages/barcode_scanner_page.dart
+++ lib/pages/barcode_scanner_page.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:inventory/services/barcode_scanner_provider.dart';
-import 'package:inventory/services/open_food_services.dart';
 import 'package:inventory/views/product_view.dart';
 
 class BarcodeScannerPage extends ConsumerStatefulWidget {
-  const BarcodeScannerPage({Key? key}) : super(key: key);
+  final Map<String, dynamic> product;
+
+  const BarcodeScannerPage(this.product, {Key? key}) : super(key: key);
 
   @override
   ConsumerState<BarcodeScannerPage> createState() => BarcodeScannerPageState();
 }
 
 class BarcodeScannerPageState extends ConsumerState<BarcodeScannerPage> {
-  String barcode = '';
-  Map<String, dynamic> product = {};
-  final openFoodServices = OpenFoodServices();
-
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -28,26 +24,9 @@ class BarcodeScannerPageState extends ConsumerState<BarcodeScannerPage> {
             textAlign: TextAlign.center,
           ),
           const SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () async {
-              final scanner = ref.read(barcodeScannerProvider);
-              final code = await scanner.scanBarcode();
-              setState(() {
-                barcode = code;
-              });
-
-              var fetchedProduct = await openFoodServices.getProduct(barcode);
-              if (fetchedProduct != null) {
-                setState(() {
-                  product = fetchedProduct;
-                });
-              }
-            },
-            child: const Text('Scan Barcode'),
-          ),
           const SizedBox(height: 30),
-          product.isNotEmpty
-              ? ProductView(product: product)
+          widget.product.isNotEmpty
+              ? ProductView(product: widget.product)
               : const Text('Product not found'),
         ],
       ),
