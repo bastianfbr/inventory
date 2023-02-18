e38b31067537414d7e8e0cd655f5c2877c34b169
bastianfbr <badmaxyt@gmail.com>
2023-02-18 12:40:00 +0100

    💄 ✨ Consume ProductNotifier and remove navbar

--- lib/app_menu.dart
+++ lib/app_menu.dart
@@ -4,21 +4,14 @@ import 'package:inventory/pages/barcode_scanner_page.dart';
 import 'package:inventory/services/barcode_scanner_provider.dart';
 import 'package:inventory/services/open_food_services.dart';
 
-class AppMenu extends ConsumerStatefulWidget {
-  const AppMenu({super.key});
+class AppMenu extends HookConsumerWidget {
+  const AppMenu({Key? key}) : super(key: key);
 
   @override
-  ConsumerState<AppMenu> createState() => AppMenuState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final openFoodServices = OpenFoodServices();
+    final productsNotifier = ref.watch(productsProvider.notifier);
 
-class AppMenuState extends ConsumerState<AppMenu> {
-  String barcode = '';
-  Map<String, dynamic> product = {};
-  final openFoodServices = OpenFoodServices();
-  int currentPageIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -26,55 +19,24 @@ class AppMenuState extends ConsumerState<AppMenu> {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         elevation: 20,
-        centerTitle: mounted,
       ),
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        selectedIndex: currentPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(
-            selectedIcon: Icon(Icons.inventory_rounded),
-            icon: Icon(Icons.inventory_outlined),
-            label: 'Inventory',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.settings_rounded),
-            icon: Icon(Icons.settings_outlined),
-            label: 'Settings',
-          )
-        ],
-      ),
-      body: <Widget>[
-        BarcodeScannerPage(product),
-        Container(),
-      ][currentPageIndex],
-      floatingActionButton: currentPageIndex == 0
-          ? FloatingActionButton(
-              onPressed: () async {
-                final scanner = ref.read(barcodeScannerProvider);
-                final code = await scanner.scanBarcode();
-                setState(() {
-                  barcode = code;
-                });
+      body: const BarcodeScannerPage(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final scanner = ref.read(barcodeScannerProvider);
+          final code = await scanner.scanBarcode();
 
-                var fetchedProduct = await openFoodServices.getProduct(barcode);
-                if (fetchedProduct != null) {
-                  setState(() {
-                    product = fetchedProduct;
-                  });
-                }
-              },
-              tooltip: 'Scan Barcode',
-              child: const Icon(
-                Icons.barcode_reader,
-                size: 30,
-              ),
-            )
-          : Container(),
+          var fetchedProduct = await openFoodServices.getProduct(code);
+          if (fetchedProduct != null) {
+            productsNotifier.addProduct(fetchedProduct);
+          }
+        },
+        tooltip: 'Scan Barcode',
+        child: const Icon(
+          Icons.barcode_reader,
+          size: 30,
+        ),
+      ),
     );
   }
 }
