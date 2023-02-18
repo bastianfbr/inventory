8f7830a47c167fcbbca0b2abdd34d084d5aa48a3
bastianfbr <badmaxyt@gmail.com>
2023-02-18 17:53:57 +0100

    ✨ Edit and delete functions

--- lib/services/products_notifier.dart
+++ lib/services/products_notifier.dart
@@ -1,10 +1,30 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:inventory/models/product.dart';
 
+final productsProvider =
+    StateNotifierProvider<ProductsNotifier, List<Product>>((ref) {
+  return ProductsNotifier();
+});
+
 class ProductsNotifier extends StateNotifier<List<Product>> {
   ProductsNotifier() : super([]);
 
   void addProduct(Product product) {
     state = [...state, product];
+  }
+
+  void editProduct(String productId, Product updatedProduct) {
+    final index = state.indexWhere((product) => product.id == productId);
+    if (index != -1) {
+      state = [
+        ...state.sublist(0, index),
+        updatedProduct,
+        ...state.sublist(index + 1),
+      ];
+    }
+  }
+
+  void deleteProduct(String productId) {
+    state = state.where((product) => product.id != productId).toList();
   }
 }
